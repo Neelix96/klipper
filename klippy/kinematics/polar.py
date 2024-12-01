9072daf68c0218cd7bd42d7334b2a52951c32a8d
nils.hensch <nils.hensch@star-dresden.de>
2024-12-01 21:57:30 +0100

    fixed max speed

--- klippy/kinematics/polar.py
+++ klippy/kinematics/polar.py
@@ -168,9 +168,10 @@ class PolarKinematics:
                 if min_dist != 0:
                     scale_radius = min_dist/self.critical_radius
                     scale_angle = abs(1.0 - (abs(180.0 - angle if angle > 90.0 else angle) / 90.0))  # From Marlin
-                    move.limit_speed((self.max_velocity/100) ** (scale_angle * scale_radius),
-                                     (self.max_accel/100) ** (scale_angle * scale_radius))
-                    logging.info("Vel: %s, Acc: %s", self.max_velocity ** (scale_angle * scale_radius), self.max_accel ** (scale_angle * scale_radius))
+                    # move.limit_speed((self.max_velocity/100) ** (scale_angle * scale_radius),
+                    #                  (self.max_accel/100) ** (scale_angle * scale_radius))
+                    move.limit_speed(1, 1)
+                    # logging.info("Vel: %s, Acc: %s", self.max_velocity ** (scale_angle * scale_radius), self.max_accel ** (scale_angle * scale_radius))
 
     def get_status(self, eventtime):
         xy_home = "xy" if self.limit_xy2 >= 0. else ""
