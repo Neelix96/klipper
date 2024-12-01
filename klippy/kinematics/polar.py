11de09fbb89185620832dfece4b8e06f1368d4d1
nils.hensch <nils.hensch@star-dresden.de>
2024-12-01 18:17:46 +0100

    fix negative speed

--- klippy/kinematics/polar.py
+++ klippy/kinematics/polar.py
@@ -167,7 +167,7 @@ class PolarKinematics:
             if min_dist <= self.critical_radius and point_within_boundary:
                 if min_dist != 0:
                     scale_radius = min_dist/self.critical_radius
-                    scale_angle = 1.0 - (abs(180.0 - angle if angle > 90.0 else angle) / 90.0)  # From Marlin
+                    scale_angle = abs(1.0 - (abs(180.0 - angle if angle > 90.0 else angle) / 90.0))  # From Marlin
                     move.limit_speed(self.max_velocity * scale_angle * scale_radius,
                                      self.max_accel * scale_angle * scale_radius)
                     logging.info("Radius: %s, Angle: %s, Vel: %s, Acc: %s", scale_radius, scale_angle, self.max_velocity, self.max_accel)
