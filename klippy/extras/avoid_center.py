4c91813383e016424aee211eef361fdb15685272
nils.hensch <nils.hensch@star-dresden.de>
2024-12-05 22:06:14 +0100

    remove exception handling

--- klippy/extras/avoid_center.py
+++ klippy/extras/avoid_center.py
@@ -284,10 +284,10 @@ class AvoidCenter:
 
     def _move_from_excluded_region(self, newpos, speed):
         #TODO: Add extrusion compensation similiar to ExludeObject
-        try:
-            self._normal_move(newpos, speed)
-        except TypeError:
-            logging.error("Pos: %s, Speed: %s", newpos, speed)
+        # try:
+        self._normal_move(newpos, speed)
+        # except TypeError:
+        #     logging.error("Pos: %s, Speed: %s", newpos, speed)
 
         # This adjustment value is used to compensate for any retraction
         # differences between the last object printed and excluded one.
